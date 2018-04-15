a0fa344a99c5b585f51ffb07c0b0df7091322f5d
Damjan Kovacic <letecnez@gmail.com>
2018-04-15 21:31:14 +0200

    Simple authorization with token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const UsersController = require('../controllers/users');
 
-router.get('/', UsersController.users_get_all);
-router.post('/', UsersController.users_create_user);
+router.get('/', checkAuth, UsersController.users_get_all);
+router.post('/', checkAuth, UsersController.users_create_user);
 router.post('/signin', UsersController.users_signin);
-router.get('/:userId', UsersController.users_get_user);
+router.get('/:userId', checkAuth, UsersController.users_get_user);
 // router.patch('/:userId', UsersController.users_update_user);
-router.delete('/:userId', UsersController.users_delete_user)
+router.delete('/:userId', checkAuth, UsersController.users_delete_user)
 
 
 module.exports = router;
