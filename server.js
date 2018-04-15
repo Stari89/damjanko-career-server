15a1c3f876457628540d35dc14e2ed25cbc3f964
Damjan Kovacic <letecnez@gmail.com>
2018-04-15 14:40:58 +0200

    Init method and environment variables checking

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3001;
+init();
 
-const server = http.createServer(app);
+/* Initialize everything */
+function init() {
+	validateEnvironmentVariables();
+	startServer();
+}
 
-server.listen(port);
+/* Checks environment variables before we start doing anything */
+function validateEnvironmentVariables() {
+	if (!process.env.DAMJANKO_CAREER_SERVER_PORT) {
+		throw new Error('DAMJANKO_CAREER_SERVER_PORT environment variable is undefined');
+	}
+	if (!process.env.DAMJANKO_CAREER_SERVER_JWT_KEY) {
+		throw new Error('DAMJANKO_CAREER_SERVER_JWT_KEY environment variable is undefined');
+	}
+	if (!process.env.DAMJANKO_CAREER_SERVER_MONGODB_URI) {
+		throw new Error('DAMJANKO_CAREER_SERVER_MONGODB_URI environment variable is undefined');
+	}
+	if (!process.env.DAMJANKO_CAREER_SERVER_MONGODB_PASSWORD) {
+		throw new Error('DAMJANKO_CAREER_SERVER_MONGODB_PASSWORD environment variable is undefined');
+	}
+}
+
+/* Starts server */
+function startServer() {
+	const port = process.env.DAMJANKO_CAREER_SERVER_PORT;
+	const server = http.createServer(app);
+	server.listen(port);
+}
